d88b129bc3d6bebacf91fc00c6fb347f19b12279
Kelvin <kelvin.gamedeveloper@gmail.com>
2015-10-01 15:29:16 -0300

    Movimentação cartesiana até um x e y. Porém, apresenta bugs

--- Pathfind/main.cpp
+++ Pathfind/main.cpp
@@ -10,18 +10,80 @@
 #include "..\..\header\campojogo.h"
 #include "..\..\header\grade.h"
 #include "..\..\header\soldado.h"
+#include "..\..\header\pagina.h"
 
 using namespace std;
 
 
 int main(){
 	
+	// Declaração de variáveis
+	CampoJogo meuCampo;
+	Pagina minhaPg;
+	bool gameLoop = true;
+	Soldado meuSold; 
+	int x, y; // Opção do jogador para movimentação
+	
 	// Inicialize a janela gráfica
 	initwindow(TELA_W,TELA_H);
-	CampoJogo meuCampo;
+	
+	// Inicializa a estrutura página
+	minhaPg.Init();
+	
+	// Troca a página atual
+	minhaPg.Troca();
+
+	// Trabalha com a página nos "bastidores"
+	minhaPg.Ativa();
+	
+	// Inicialização do campo de jogo a partir de um arquivo de coordenadas
 	meuCampo.Init("mapa01.txt");
+	
+	// Mostra campo de jogo
 	meuCampo.Mostrar();
-	while(!kbhit());
+	
+	// Inicializa o soldado
+	meuSold.Init("Chara");
+	
+	// Mostra soldado
+	meuSold.Show();	
+	
+	// Deixa a página visual
+	minhaPg.Visual();
+	
+
+	
+	//Loop do jogo
+	while(gameLoop == true){
+		
+		// Troca e ativa uma nova página para modifcações nos gráficos
+		minhaPg.Troca();
+		minhaPg.Ativa();
+		
+		// Limpa a tela
+		cleardevice();
+		
+		// Mostra campo de jogo
+		meuCampo.Mostrar();
+	
+		/// Teste - Move-se até o destino especificado
+		cout << endl << "x = " << meuSold.x << " | y = " << meuSold.y;
+	if (meuSold.MovUntil(meuSold.X + (TILE_W * 1) , meuSold.Y - (TILE_H * 2))){
+	/*	if(meuSold.MovUntil(120,800)){*/
+		
+			meuSold.MovUntil(meuSold.x, 64 );
+		}
+		
+		// Mostra soldado
+		meuSold.Show();
+		
+		//Deixa a página visual
+		minhaPg.Visual();
+				
+		// Delay de FPS
+		delay(FPS);
+		
+	}
 	closegraph();
 	return 0;	
 }
