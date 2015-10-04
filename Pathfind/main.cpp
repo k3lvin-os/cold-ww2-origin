971674c46db0b0d10ee46562dec22a1935fe9810
Kelvin <kelvin.gamedeveloper@gmail.com>
2015-10-04 20:54:16 -0300

    Modulariazação do código com a função IA da estrutura Soldado

--- Pathfind/main.cpp
+++ Pathfind/main.cpp
@@ -23,7 +23,6 @@ int main(){
 	bool gameLoop = true;
 	Soldado meuSold; 
 	int x, y; // Opção do jogador para movimentação
-	bool visivel = false; // Indica se o soldado chegou ao ponto visivel (hora da pathfind)
 	
 	// Inicialize a janela gráfica
 	initwindow(TELA_W,TELA_H);
@@ -65,60 +64,9 @@ int main(){
 		// Mostra campo de jogo
 		meuCampo.Mostrar();
 		
-		// Se o soldado não estiver no "ponto cego"
-		if(meuSold.posCego == false){
-			
-			// Se o soldado não souber para onde ir
-			if(meuSold.movNUntil == false){
-				
-				// Calcula as coordendas do ponto cego
-				x = meuSold.X + (TILE_W * 21);
-				y = meuSold.Y - (TILE_H * 3);
-				
-				// Define o ponto cego como destino
-				meuSold.MovUntil(x,y);
-			}
-			
-			// Movimenta-se até o ponto cego
-			meuSold.posCego = meuSold.MovUntil();
-		} 
-		// Se o soldado estiver no "ponto cego"
-		else{
-			
-			// Se o soldado não estiver no ponto visível
-			if(visivel == false){
-					
-					// Se o soldado não souber para onde ir
-					if(meuSold.movNUntil == false){
-						
-						// Define a coordenada do ponto visível
-						meuSold.MovUntil(meuSold.x, 64);
-					} 
-					
-					// Movimente o soldado até o destino
-					visivel = meuSold.MovUntil();
-			} 
-			
-			// Se o soldado chegou a área visível
-			else{
-				
-				// Se o soldado não chegou ao destino
-				if(meuSold.dest  == false){
-					
-					// Se o soldado não souber para onde ir
-					if(meuSold.movNUntil == false){
-						
-						// Usa a Pathfind
-						meuSold.dest = 
-						meuSold.MoveDest(meuCampo,DEST1_X,DEST1_Y);
-					} 
-					
-					// Movimenta-se até o ponto definido
-					meuSold.MovUntil();
-				}
-				
-			}		
-		}
+		// Utiliza a IA do soldado
+		meuSold.IA(meuCampo);
+		
 		// Mostra soldado
 		meuSold.Show();
 		
